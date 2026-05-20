6e8849dcb6dcb5a537c4a706958da3e16ce70f69
Ziad Amr <Zyadamr177@gmail.com>
2026-05-20 20:24:56 +0300

    feat: add save and load system

--- systems/saveSystem.js
+++ systems/saveSystem.js
@@ -1,46 +1,31 @@
 const fs = require("fs");
 const { allInGameItems } = require("../factories/createItem");
 const { allInGameEnemies } = require("../factories/createEnemy");
-const playerData = require("../data/player");
-let player = playerData.player;
+const gameState = require("../state/gameState");
 
 // =========== SAVE SYSTEM ===========
-function savePlayer(player) {
-  let playerSave = {
-    name: player.name,
-    health: player.health,
-    maxHealth: player.maxHealth,
-    coins: player.coins,
-    exp: player.exp,
-    level: player.level,
-    inventory: player.inventory,
-  };
+function saveGamestate(gamestate) {
+  let gamestateSave = gamestate;
 
-  fs.writeFile("../data/player.json", JSON.stringify(playerSave), (err) => {
+  fs.writeFile("./data/gamestate.json", JSON.stringify(gamestateSave), (err) => {
     if (err) {
       console.log(err);
     } else {
-      console.log("Player data saved");
+      console.log("Game saved");
     }
   });
 }
 
-function loadPlayer() {
-  fs.readFile("../data/player.json", "utf8", (err, data) => {
+function loadGamestate() {
+  fs.readFile("./data/gamestate.json", "utf8", (err, data) => {
     if (err) {
       console.log(err);
     } else {
-      let playerData = JSON.parse(data);
-      player.name = playerData.name;
-      player.health = playerData.health;
-      player.maxHealth = playerData.maxHealth;
-      player.coins = playerData.coins;
-      player.exp = playerData.exp;
-      player.level = playerData.level;
-      player.inventory = playerData.inventory;
+      let gamestateData = JSON.parse(data);
+      Object.assign(gameState, gamestateData);
       console.log("Player data loaded");
     }
   });
 }
 
-module.exports = { savePlayer, loadPlayer };
+module.exports = { saveGamestate, loadGamestate };
