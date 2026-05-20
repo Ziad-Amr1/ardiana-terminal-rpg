485d21a82f5b14f9b7562428ed0d04b587babea0
Ziad Amr <Zyadamr177@gmail.com>
2026-05-20 20:24:10 +0300

    feat: add weighted enemy spawning system

---
+++ systems/spawningSystem.js
@@ -0,0 +1,38 @@
+const { randomNumber } = require("../utitls/randoms");
+const { allInGameEnemies } = require("../factories/createEnemy");
+
+// =========== SPAWNING SYSTEM ===========
+let choiceEnemy = (area) => {
+
+  if (!area || !area.enemies) {
+   return null;
+  }
+
+let enemies = area.enemies;
+
+let getTotalWeight = (enemies) => {
+  let totalWeight = 0;
+  for (let i = 0; i < enemies.length; i++) {
+    totalWeight += enemies[i].weight;
+  }
+  return totalWeight;
+};
+
+let totalWeight = getTotalWeight(enemies);  
+
+
+  let spawn = randomNumber(1, totalWeight);
+  let currentWeight = 0;
+  for (let i = 0; i < enemies.length; i++) {
+    currentWeight += enemies[i].weight;
+    if (spawn <= currentWeight) {
+      let enemy = allInGameEnemies.find(
+        (enemy) => enemy.id === enemies[i].enemyId
+      );
+      
+      return structuredClone(enemy);
+    }
+  }
+};
+
+module.exports = { choiceEnemy };
